be158df8fb9902ce3faf66a727300b986ad554ea
Janani93 <kjanani1993@gmail.com>
2019-08-13 15:35:24 +0530

    initial commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function category()
+    {
+        return view('categories.category');
+    }
+
+    public function addCategory(Request $request)
+    {
+        return $request->input('category');
+    }
+}
---
+++ resources/views/categories/category.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Category</div>
+
+                <div class="panel-body">
+                        <form class="form-horizontal" method="POST" action="{{ url('/addCategory') }}">
+                            {{ csrf_field() }}
+    
+                            <div class="form-group{{ $errors->has('category') ? ' has-error' : '' }}">
+                                <label for="category" class="col-md-4 control-label">Enter Category</label>
+    
+                                <div class="col-md-6">
+                                    <input id="category" type="category" class="form-control"
+                                     name="category" value="{{ old('category') }}" required autofocus>
+    
+                                    @if ($errors->has('category'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('category') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>                           
+                                    
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Add
+                                    </button>
+                                        
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
